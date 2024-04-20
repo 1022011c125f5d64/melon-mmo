4cdcab0f34b4931f4cd467835c872dfa1fb90f33
ChaosCodex <imaginedragonslee@163.com>
2024-04-20 22:48:36 +0800

    .

--- server/server.c
+++ server/server.c
@@ -1,5 +1,11 @@
 #include <stdio.h>
 #include <pthread.h>
+#include "include/unit.h"
+
+// global variables
+struct unit_s* __npcs;
+struct unit_s* __creatues;
+struct unit_s* __players;
 
 static void _thread_network() {
     
